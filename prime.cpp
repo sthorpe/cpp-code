3e74ea363f542f88aa1200a06d2322ec6239ca30
Scott Thorpe <sthorpe@gmail.com>
2018-09-25 09:30:28 -0400

    Loops and functions lession. Nothing really hard on this at all.

--- prime.cpp
+++ prime.cpp
@@ -1,34 +1,112 @@
 /* 
-  Overloading functions
+  Loops and functions
 */
 
 #include <iostream>
+#define PI 3.14
 
 using namespace std;
 
-int power(int, int);
-double power(double, int);
+void initMenu();
+void menuDecision(int);
+double areaCircle(double);
+double areaSquare(double);
+double areaRectangle(double, double);
+double areaTriangle(double, double);
+//const double PI = 3.14
 
 int main()
 {
-    cout << power(2.5, 3) << endl;
+    int choice;
+    char cont;
+    do
+    {
+        system("cls"); // Clear screen
+        initMenu();
+
+        cin >> choice;
+
+        menuDecision(choice);
+
+        do
+        {
+            cout << "Do you want to try again? (Y/N)" << endl; 
+            cin >> cont;
+        } while(cont != 'y' && cont != 'Y' && cont != 'n' && cont != 'N');
+
+    } while(cont == 'y' || cont == 'Y');
+
     return 0;
 }
 
-int power(int b, int e)
+void initMenu()
 {
-    int tmp = b;
-    while(e-- > 1)
-        b *= tmp;
-
-    return b;
+    cout << "Enter option: " << endl;
+    cout << "1. Circle" << endl;
+    cout << "2. Square" << endl;
+    cout << "3. Rectangle" << endl;
+    cout << "4. Triangle" << endl;
 }
 
-double power(double b, int e)
+void menuDecision(int choice)
 {
-    double tmp = b;
-    while(e-- > 1)
-        b *= tmp;
+    switch(choice)
+    {
+        double r, a, b, h;
+        case 1:
+            cout << "What is the radius? " << endl;
+            cin >> r;
+            areaCircle(r);
+            break;
+        case 2:
+            cout << "Enter the side of the square? " << endl;
+            cin >> a;
+            areaSquare(a);        
+            break;
+        case 3:
+            cout << "Enter the width and height of a rectangle: " << endl;
+            cin >> a >> b;
+            areaRectangle(a, b);
+            break;
+        case 4:
+            cout << "What is the base and height of a triangle? " << endl;
+            cin >> a >> h;
+            areaTriangle(a, h);        
+            break;
+        default:
+            cout << "You didn't choose any of the above options" << endl;
+    }
+}
 
-    return b;
+double areaCircle(double r)
+{
+    double result = PI * r * r;
+
+    cout << "The area of a circle that radius is: " << r << " = " << result << endl;
+
+    return result;
+}
+double areaSquare(double a)
+{
+    double result = a * a;
+
+    cout << "The area of a square that side is: " << a << " = " << result << endl;
+
+    return result;
+}
+double areaRectangle(double a, double b)
+{
+    double result = a * b;
+
+    cout << "The area of a rectangle that first side is " << a << " and whos second side is " << b << " = " << result << endl;
+
+    return result;
+}
+double areaTriangle(double a, double h)
+{
+    double result = (1/2.0) * a * h;
+
+    cout << "The area of a triangle that base is " << a << " and height is " << h << " = " << result << endl;
+
+    return result;
 }
