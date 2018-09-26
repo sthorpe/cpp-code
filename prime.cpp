53c07236dc90cd73913a486192347dea93fb6d76
Scott Thorpe <sthorpe@gmail.com>
2018-09-26 14:26:16 -0400

    Created reference functions and its cool. They take on behavoir!

--- prime.cpp
+++ prime.cpp
@@ -1,19 +1,35 @@
 /*
-    Reference Variables
-    A nickname for a variable
+    Reference Variables in functions
 */
 
 #include <iostream>
 
 using namespace std;
 
+int & swap(int &, int &);
+
 int main()
 {
-    string name = "Scott";
-    cout << "The value of our variable: " << name << " address " << &name << endl;
-    const string &nr_6 = name;
-    cout << "The value of our variable: " << nr_6 << " address " << &nr_6 << endl;
-    name = "Caitlin";
-    cout << "The value of our variable: " << nr_6 << " address " << &nr_6 << endl;
+    int a = 10;
+    int b = 20;
+
+    int &c = swap(a, b);
+
+    c = 100;
+
+    cout << "a: " << a << endl;
+    cout << "b: " << b << endl;
+    cout << "c: " << c << endl;
+
     return 0;
+}
+int & swap(int &x, int &y)
+{
+    int t;
+
+    t = x; // 10
+    x = y; // 20
+    y = t; // 10
+
+    return x;
 }
