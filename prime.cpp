0ca017c7a2ad066b337cd6d4851d598ef8fd0322
Scott Thorpe <sthorpe@gmail.com>
2018-09-27 11:07:50 -0400

    Pointers!

--- prime.cpp
+++ prime.cpp
@@ -1,35 +1,52 @@
 /*
-    Reference Variables in functions
+    Pointers are ordinary variables that can store variables address
 */
 
 #include <iostream>
 
 using namespace std;
 
-int & swap(int &, int &);
-
 int main()
 {
-    int a = 10;
-    int b = 20;
+    int var = 5;
+    int a = 20;
 
-    int &c = swap(a, b);
+    cout << &var << endl;
 
-    c = 100;
+    // int *p; // * here is just to tell the compiler that p is a pointer.
 
+    int *p = &var;
+
+    cout << *p << endl; // Use * on a pointer to get the value
+
+    //*p = 20;
+    p = &a;
+    *p = 60;
+    
+    cout << "var: " << var << endl;
+    cout << "*p: " << *p << endl;
     cout << "a: " << a << endl;
-    cout << "b: " << b << endl;
-    cout << "c: " << c << endl;
+
+    int * const p_const = &a; // this is a pointer that has to be initalized when defined, because it cannot change after defining the thing it is pointing to.
+
+    const int * p_2 = &a;
+
+    const int * const p_3 = &a;
+
+    cout << endl << endl << endl;
+
+    int ordinary_var = 10;
+
+    int *ordinary_p = &ordinary_var;
+
+    cout << "ordinary_var: " << ordinary_var << endl; //integer value
+    cout << "&ordinary_var: " << &ordinary_var << endl; //address 
+    cout << "ordinary_p: " << ordinary_p << endl; //address
+    cout << "*ordinary_p: " << *ordinary_p << endl; //integer value from pointed place (ordinary_var)
+    cout << "&ordinary_p: " << &ordinary_p << endl; //address of pointer itself
+
+    int ** p_pointing_to_address_of_pointer = &ordinary_p;
+    cout << "p_pointing_to_address_of_pointer: " << &p_pointing_to_address_of_pointer << endl; 
 
     return 0;
-}
-int & swap(int &x, int &y)
-{
-    int t;
-
-    t = x; // 10
-    x = y; // 20
-    y = t; // 10
-
-    return x;
 }
