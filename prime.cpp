4ae3cd4d80304a4b225555572b2cea94f587d14a
Scott Thorpe <sthorpe@gmail.com>
2018-10-03 13:06:09 -0400

    Type casting

--- prime.cpp
+++ prime.cpp
@@ -1,48 +1,33 @@
 /*
-    Pseudo-Random number generator
+    TYPE CASTING
 */
 
 #include <iostream>
 
 using namespace std;
 
-void lottery(int, int);
-
 int main()
 {
-    // srand(time(NULL));
+    double var = 5.5;
 
-    // int nr = rand() % 3;
+    int a = 5;
+    int b = 7;
 
-    // cout << nr << endl;
+    cout << (int) var << endl; // explicit way
 
-    lottery(49, 6);
+    cout << (double)a / b << endl;
+
+    int x = 10; // implicit way of casting
+    short y = x; // You can assign the value but it ends up being wrong
+
+    cout << y << endl;
+
+    cout << int(var) << endl; // c version of casting
+    cout << static_cast<int>(var) << endl; // c++ way of casting
+
+    char ch = 'k';
+
+    cout << (int)ch << endl;
+
     return 0;
-}
-
-void lottery(int total_balls, int balls_to_draw)
-{
-    if (total_balls < balls_to_draw)
-        return;
-    srand(time(NULL)); // Seed
-
-    int *balls = new int[balls_to_draw];
-
-    for (int i = 0; i < balls_to_draw; i++)
-    {
-        balls[i] = rand() % total_balls + 1; // [1, 49] modulus gives us the proper range
-
-        for (int j = 0; j < i + 1; j++)
-        {
-            if (balls[i] == balls[j] && i != j) // Possible problem is balls[0] == balls[0] which would go forever
-            {
-                i--;
-                break;
-            }
-            else if (j == i)
-                cout << balls[i] << endl;
-        }
-    }
-
-    delete[] balls;    
 }
