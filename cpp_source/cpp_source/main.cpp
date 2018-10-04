a1f9e04307d81ed36b1ff68ca0aaa8d5d402d275
Scott Thorpe <sthorpe@gmail.com>
2018-10-04 13:59:02 -0400

    Data structures and pointers

--- cpp_source/cpp_source/main.cpp
+++ cpp_source/cpp_source/main.cpp
@@ -5,16 +5,58 @@
 //  Created by Scott Thorpe on 10/4/18.
 //  Copyright © 2018 Scott Thorpe. All rights reserved.
 //
+/*
+ Data Structres
+*/
 
-#include "ourfirstlibrary.h"
+#include <iostream>
 
 using namespace std;
 
-#define PI 3.14
+struct personalData
+{
+    string name;
+    string surname;
+    string telephoneNumber;
+    short age;
+}a,b;
+
+void test(personalData *);
 
 int main() {
     
-    showHelp();
-    cout << a << endl;
+    personalData person[5];
+    a.age = 10;
+    person[0].name = "Scott";
+    person[1].name = "Ralph";
+    person[0].surname = "sthorpe";
+    person[0].telephoneNumber = "3855555555";
+    person[0].age = 38;
+    
+    cout << person[0].name << endl;
+    cout << person[0].surname << endl;
+    cout << person[0].telephoneNumber << endl;
+    cout << person[0].age << endl;
+    
+    cout << (*person).name << endl;
+    cout << (*(person+1)).name << endl;
+    cout << (*&person[0]).name << endl;
+    cout << person -> name << endl;
+    cout << (person+1) -> name << endl;
+    
+    cout << a.age << endl;
+    
+    personalData *p = &person[0];
+    
+    cout << p -> age << endl;
+    
+    test(p);
+    
+    cout << p->name << endl;
+    
     return 0;
+}
+void test(personalData *person)
+{
+    person -> name = "Caitlin";
 }
