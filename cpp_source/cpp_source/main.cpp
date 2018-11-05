e7167e68f1155bab34af3ad7bf0e267a2658a154
Scott Thorpe <sthorpe@gmail.com>
2018-11-05 13:45:08 -0400

    Exceptions

--- cpp_source/cpp_source/main.cpp
+++ cpp_source/cpp_source/main.cpp
@@ -1,49 +1,48 @@
 #include <iostream>
-#include "point.h"
 
 using namespace std;
-/* function templates generalization*/
+/* Exceptions - handling exceptions */
 
-void operationOnPoints();
-
-template<typename T, typename T2>
-T add(T var1, T2 var2)
+class DivisionByZeroException
 {
-    return var1 + var2;
+public:
+    void getErrorMessage() { cout << "Error: do not divide by zero" << endl; }
+};
+
+double division(double a, double b) throw(DivisionByZeroException) // Just for information about which exception
+{
+    if (b == 0)
+        throw DivisionByZeroException();
+    return a / b;
 }
-/*
- template<>
- Point2D add(Point2D var1, Point2D var2)
- {
- Point2D tmp;
- 
- tmp.setX(var1.getX() + var2.getX());
- tmp.setY(var1.getY() + var2.getY());
- 
- return tmp;
- }
- */
 int main()
 {
-    operationOnPoints();
+    double a = 5;
+    double result;
+    try
+    {
+        result = division(5, 0);
+        cout << "The result is " << result << endl;
+//        a *= 10;
+//        if (a == 50)
+//            throw a;
+    }
+    catch(DivisionByZeroException e)
+    {
+        e.getErrorMessage();
+    }
+//    catch(double e)
+//    {
+//        cout << "a cannot be equal to " << a << endl;
+//    }
+//    catch(...)
+//    {
+//        cout << "this will catch anything " << a << endl;
+//    }
     
-    //cout << add<double, double>(2, 5.6) << endl;
-    
-    
-    // cout << static_cast<int>(6.5) << endl;
+    cout << "Yeah it still runs this!" << endl;
     
     return 0;
 }
-void operationOnPoints()
-{
-    Point2D p1(10, 67);
-    Point2D p2(50, 3); // 60,70
-    
-    Point2D sum = add(p1,p2);
-    
-    cout << sum.getX() << endl;
-    cout << sum.getY() << endl;
-    
-    
-}
+
 
