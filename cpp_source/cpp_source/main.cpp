cc6ec490dbb319413b3269cdbf7e20e328add4c7
Scott Thorpe <sthorpe@gmail.com>
2018-11-12 07:59:28 -0400

    Cleaning things up

--- cpp_source/cpp_source/main.cpp
+++ cpp_source/cpp_source/main.cpp
@@ -1,46 +1,12 @@
 #include <iostream>
 
 //using namespace std;
-/* Namespaces */
+/* Streams */
 
-namespace A
-{
-    int a = 20;
-}
-
-namespace B
-{
-    int a = 60;
-}
-
-namespace mySpace
-{
-    class MyNewLine
-    {
-        std::string text;
-    public:
-        MyNewLine(std::string text = "\n\n\n") { this->text = text; }
-        std::string toString()
-        {
-            return text;
-        }
-    };
-    std::ostream & operator<<(std::ostream & out, MyNewLine & obj)
-    {
-        return out << obj.toString();
-    }
-    MyNewLine endl;
-}
-
-using namespace mySpace;
+using namespace std;
 
 int main()
 {
-    int a = 50;
-    
-    std::cout << a << mySpace::endl;
-    std::cout << A::a << std::endl;
-    std::cout << B::a << endl;
     return 0;
 }
 
