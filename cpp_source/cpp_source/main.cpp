ac6a8038c7f615ecdebe24542dc97b8e3cbea57d
Scott Thorpe <sthorpe@gmail.com>
2019-07-03 10:19:06 -0600

    Working with file streams.

--- cpp_source/cpp_source/main.cpp
+++ cpp_source/cpp_source/main.cpp
@@ -8,15 +8,31 @@ using namespace std;
 int main()
 {
     fstream myFileHandler;
-    myFileHandler.open("test.txt");
-    int x;
+    myFileHandler.open("test.txt", ios::out | ios::app);
+    
+    /*
+     ios::in - INPUT - READING
+     ios::out - OUTPUT - WRITE TO FILE, if there is no file then create, if there is a filethen truncate it (remove content) unless it occurs with ios::in
+     ios::trunc - TRUNCATE - it is truncating the file (cutting everything inside)
+     ios::ate - At The End - sets pointer at the end of file - The place of a pointer can be changed in that mode, it is possible to read and write in that mode.
+     ios::app - Append - The content is added at the end of file. It is not possible to remove content
+     ios::binary - opens the file as a binary file
+    */
+    
+    /*
+     DEFAULT MODE (FLAGS)
+     fstream - ios::out | ios::in
+    */
 
     if (myFileHandler.is_open())
     {
         cout << "The file was opened properly" << endl;
-        myFileHandler << "This is a test :)" << endl;
-        myFileHandler.close();
+        myFileHandler << "This is a test :)/n";
+        myFileHandler << "This is a test :)/n";
+        
     }
+    else
+        cout << "The file hasn't been opened properly";
     
     return 0;
 }
